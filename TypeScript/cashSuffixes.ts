5a29cd8c59e0be99a51d912d2991023e738e0837
wuw.sh <87937525+wuw-sh@users.noreply.github.com>
2022-08-24 20:44:21 +0700

    Add files via upload

---
+++ TypeScript/cashSuffixes.ts
@@ -0,0 +1,28 @@
+export function cashSuffixes(value: number, turnOff: boolean): number | string {
+    try {
+        if (!turnOff) return numberWithCommas(value)
+        const selectType = Math.log10(Math.abs(value)) / 3 | 0;
+        if (selectType === 0) return value
+        const types = ['', 'k', 'M', 'B'];
+        let scaled = (value / Math.pow(10, selectType * 3)).toString(), number: number | string
+        const split = scaled.split('.'), int = split[0], decimal = split[1]
+        if (decimal === undefined) {
+            number = int
+        } else if (decimal.charAt(1) === '0') {
+            number = int + '.' + decimal.slice(0, 1)
+        } else {
+            number = int + '.' + decimal.slice(0, 2)
+        }
+        return number + types[selectType];
+
+        function numberWithCommas(value: number): string {
+            let x = value.toString()
+            const pattern = /(-?\d+)(\d{3})/;
+            while (pattern.test(x))
+                x = x.replace(pattern, "$1,$2");
+            return x;
+        }
+    } catch (e) {
+        return value
+    }
+}
