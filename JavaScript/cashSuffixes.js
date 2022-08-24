8f8079f43212a2b10c9f121d076b18454914c34a
wuwsh <wuw6248@gmail.com>
2022-08-25 00:05:32 +0700

    add tsconfig.json

--- JavaScript/cashSuffixes.js
+++ JavaScript/cashSuffixes.js
@@ -1,27 +1,22 @@
-export function cashSuffixes(value, turnOff) {
+export function cashSuffixes(value, decimalLength = 2) {
     try {
-        if (!turnOff) return numberWithCommas(value);
         const selectType = Math.log10(Math.abs(value)) / 3 | 0;
         if (selectType === 0) return value;
         const types = ['', 'k', 'M', 'B'];
+        //You can use this: const types = ['', 'k', 'M', 'B', 'T', 'qd', 'Qn', 'sx', 'Sp', 'O', 'N'];
         let scaled = (value / Math.pow(10, selectType * 3)).toString(), number;
         const split = scaled.split('.'), int = split[0], decimal = split[1];
-        if (decimal === undefined) {
-            number = int;
-        } else if (decimal.charAt(1) === '0') {
-            number = int + '.' + decimal.slice(0, 1);
-        } else {
-            number = int + '.' + decimal.slice(0, 2);
-        }
+        if (decimal === undefined) number = int;
+        else number = int + '.' + decimal.slice(0, decimalLength);
         return number + types[selectType];
-
-        function numberWithCommas(value) {
-            let x = value.toString();
-            const pattern = /(-?\d+)(\d{3})/;
-            while (pattern.test(x)) x = x.replace(pattern, "$1,$2");
-            return x;
-        }
     } catch (e) {
         return value;
     }
 }
+
+//console.log(cashSuffixes(50000000000)) Output: 50B
+//console.log(cashSuffixes(50060000000)) Output: 50.06B
+//console.log(cashSuffixes(50500013502)) Output: 50.50B
+//console.log(cashSuffixes(50500013502, 5)) Output: 50.50001B
+//console.log(cashSuffixes(50568200000, 10)) Output: 50.5682BB
+//console.log(cashSuffixes(50000000000)) Output: 50B
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# cashSuffixes-TypeScript
-cash suffixes with TypeScript function
+# cashSuffixes TypeScript
+
+### function ย่อค่าเงิน TypeScript และ JavaScript
+
+**Function TypeScript [cashSuffixes.ts](TypeScript/cashSuffixes.ts)<br>
+Function TypeScript [cashSuffixes.js](JavaScript/cashSuffixes.js)**
+
+- สมมุต cashSuffixes(0) จะได้ผลลัพธ์ 0
+- สมมุต cashSuffixes(10) จะได้ผลลัพธ์ 16
+- สมมุต cashSuffixes(100) จะได้ผลลัพธ์ 195
+- สมมุต cashSuffixes(1200) จะได้ผลลัพธ์ 1.2k
+- สมมุต cashSuffixes(55000)  จะได้ผลลัพธ์ 55k
+- สมมุต cashSuffixes(956243) จะได้ผลลัพธ์ 956.24k
+- สมมุต cashSuffixes(1060715) จะได้ผลลัพธ์ 1.06M
+- สมมุต cashSuffixes(5054098846) จะได้ผลลัพธ์ 5.05B
+
+1,000^ = k
+
+1,000,000^ = M
+
+1,000,000,000^ = B
+
+เพิ่มค่าเงินตรง types ได้เท่าไหร่ก็ได้ หรือจะเปลี่ยนก็ได้ เช่น
+const types = ['', 'k', 'M', 'B', 'T', 'qd', 'Qn', 'sx', 'Sp', 'O', 'N'];
+
+
+wiki fandom: [Cash Suffixes](https://minershaven.fandom.com/wiki/Cash_Suffixes)
